0a6eb11347b7bb6c71341f6b953bb743bc2b6fd5
Omnya Ellethy <147027602+OmnyaEllethy-SK@users.noreply.github.com>
2025-05-27 15:00:49 +0300

    Create projects.ts

---
+++ docs/apps/web/src/config/projects.ts
@@ -0,0 +1,55 @@
+import type { DocsConfig } from '@/lib/opendocs/types/Projects'
+
+export const docsConfig: DocsConfig = {
+  mainNav: [
+    {
+      href: '/projects',
+      title: {
+        en: 'Projects',
+        pt: 'Projects',
+      },
+    },
+  ],
+
+  sidebarNav: [
+    {
+      title: {
+        en: 'Project List',
+        pt: 'lista de projetos',
+      },
+      items: [
+        {
+          href: '/projects/project-A',
+          title: {
+            en: 'Project A',
+            pt: 'Projeto A',
+          },
+          items: [
+            {
+              href: '/projects/project-A/overview',
+              title: {
+                en: 'Overview',
+                pt: 'visão geral',
+              },
+            },
+            {
+              href: '/projects/project-A/environments',
+              title: {
+                en: 'Environments',
+                pt: 'ambiente',
+              },
+            },
+          ],
+        },
+        {
+          href: '/projects/Project-B',
+          title: {
+            en: 'Project B',
+            pt: 'Projeto B',
+          },
+          items: [],
+        },
+      ],
+    },
+  ],
+} as const
